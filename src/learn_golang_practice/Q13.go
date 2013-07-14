7d399d2d4c18e915c0c5b467138f40eebd92558a
Artisan <huangrui@nibirutech.com>
2013-07-14 19:04:40 +0800

    mix func

--- src/learn_golang_practice/Q13.go
+++ src/learn_golang_practice/Q13.go
@@ -25,8 +25,29 @@ func min(arr []int) int {
 	return min
 }
 
+func mix(arr []int, mark string) int {
+	var return_value int
+	return_value = arr[0]
+	if mark == "max" {
+		for _, i := range arr {
+			if i > return_value {
+				return_value = i
+			}
+		}
+	} else {
+		for _, i := range arr {
+			if i < return_value {
+				return_value = i
+			}
+		}
+	}
+	return return_value
+}
+
 func main() {
 	arr := []int{5,6,71,23,46,9,234,64}
 	fmt.Printf("In arr max is %d\n", max(arr))
 	fmt.Printf("In arr min is %d\n", min(arr))
+	fmt.Printf("In arr in mix func min is %d\n", mix(arr, "min"))
+	fmt.Printf("In arr in mix func max is %d\n", mix(arr, "max"))
 }
